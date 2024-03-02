b886e20f999554ccf893b37a89acee8e08a3fb42
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2024-03-02 14:22:20 +0500

    Подключил бд

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,8 @@
+import prisma from '@/db'
+
 export const dynamic = 'force-dynamic'
 
 export async function POST(request: Request) {
-  return Response.json(await request.json())
+  const newMr = await prisma.mergeRequest.create({ data: {} })
+  return Response.json(newMr)
 }
