cbf6fbdebaac93fc19462bf859d8a739175558d1
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2024-03-02 16:10:35 +0500

    тест добавления статуса драфт

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,5 +3,21 @@
 export const dynamic = 'force-dynamic'
 
 export async function POST(request: Request) {
+  const data = await request.json()
+  if (data.object_attributes.action === 'open') {
+    await fetch(
+      `https://git.yamal-media.ru/api/v4/projects/${data.project.id}/merge_requests${data.object_attributes.iid}`,
+      {
+        method: 'POST',
+        headers: {
+          'PRIVATE-TOKEN': process.env.GIT_LAB_TOKEN ?? '',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: 'DRAFT' + ' ' + data.data.object_attributes.title,
+        }),
+      },
+    )
+  }
   return Response.json({ isOk: true, body: await request.json() })
 }
