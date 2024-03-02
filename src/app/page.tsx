e49befd3b814753e54ee7efaa77d857331319cbc
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2024-03-02 21:15:03 +0500

    добавил список мров

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import prisma from '@/db'
+import { format } from '@formkit/tempo'
+
+export const dynamic = 'force-dynamic'
+
+async function getData() {
+  return prisma.mergeRequest.findMany()
+}
+
+export default async function Page() {
+  const data = await getData()
+
   return (
-    <main className=''>
-      <h1>Merge request automation</h1>
+    <main className='flex flex-col space-y-4 p-4'>
+      {data.map(item => (
+        <div key={item.id} className='grid grid-cols-5 gap-x-2 border p-4'>
+          <div className='break-words'>{item.url}</div>
+          <div className='break-words'>
+            {format(item.developmentAt, { date: 'full', time: 'short' }, 'ru')}
+          </div>
+          <div className='break-words'>
+            {item.reviewAt
+              ? format(item.reviewAt, { date: 'full', time: 'short' }, 'ru')
+              : 'Этап не начат'}
+          </div>
+          <div className='break-words'>
+            {item.testingAt
+              ? format(item.testingAt, { date: 'full', time: 'short' }, 'ru')
+              : 'Этап не начат'}
+          </div>
+          <div className='break-words'>
+            {item.finishAt
+              ? format(item.finishAt, { date: 'full', time: 'short' }, 'ru')
+              : 'Этап не начат'}
+          </div>
+        </div>
+      ))}
     </main>
   )
 }
