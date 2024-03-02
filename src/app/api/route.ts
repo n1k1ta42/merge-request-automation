d5eaeacfe1dcbe88265d8e07dee946396fd71720
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2024-03-02 20:33:18 +0500

    внедрил axios

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -30,43 +30,48 @@ export async function POST(request: Request) {
     )
   }
 
+  // if (
+  //   data.object_attributes.action === 'update' &&
+  //   !data.object_attributes.title.startsWith('Draft:')
+  // ) {
+  //   const mergeRequest = await prisma.mergeRequest.findFirst({
+  //     where: {
+  //       AND: [
+  //         {
+  //           projectId: data.object_attributes.target_project_id,
+  //         },
+  //         {
+  //           iid: data.object_attributes.iid,
+  //         },
+  //       ],
+  //     },
+  //   })
+  //
+  //   if (mergeRequest) {
+  //     await prisma.mergeRequest.update({
+  //       where: {
+  //         id: mergeRequest.id,
+  //       },
+  //       data: {
+  //         reviewAt: new Date(),
+  //       },
+  //     })
+  //   }
+  //
+  //   await api.put(
+  //     getUpdateMrApiUrl(
+  //       data.object_attributes.target_project_id,
+  //       data.object_attributes.iid,
+  //     ),
+  //     JSON.stringify({
+  //       reviewer_ids: [3],
+  //     }),
+  //   )
+  // }
   if (
     data.object_attributes.action === 'update' &&
     !data.object_attributes.title.startsWith('Draft:')
   ) {
-    const mergeRequest = await prisma.mergeRequest.findFirst({
-      where: {
-        AND: [
-          {
-            projectId: data.object_attributes.target_project_id,
-          },
-          {
-            iid: data.object_attributes.iid,
-          },
-        ],
-      },
-    })
-
-    if (mergeRequest) {
-      await prisma.mergeRequest.update({
-        where: {
-          id: mergeRequest.id,
-        },
-        data: {
-          reviewAt: new Date(),
-        },
-      })
-    }
-
-    await api.put(
-      getUpdateMrApiUrl(
-        data.object_attributes.target_project_id,
-        data.object_attributes.iid,
-      ),
-      JSON.stringify({
-        reviewer_ids: [3],
-      }),
-    )
   }
   return Response.json({ isOk: true })
 }
